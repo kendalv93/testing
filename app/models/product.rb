a3d06297b3234a45829fc6d8ae837d74ac7c08c5
speerj <johann.speer@gmail.com>
2015-10-05 16:34:06 -0500

    added tests, changed model to demonstrate some rails tricks

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,19 +3,22 @@ class Product < ActiveRecord::Base
 
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
+      puts row.inspect
+      product_hash = row.to_hash
+      product = Product.find_or_initialize_by(id: product_hash['id'])
 
-      product_hash = row.to_hash # exclude the price field
-      product = Product.where(id: product_hash["id"])
-
-      if product.count == 1
-        product.first.update_attributes(product_hash.except("price"))
-      else
-        Product.create!(product_hash)
-      end # end if !product.nil?
+      product.update(product_hash)
+      product.save
     end # end CSV.foreach
   end # end self.import(file)
 
 
+  #exclude price on update
+  def update(args)
+    #deletes param if the key is :price and the object has persisted(been saved before)
+    args.delete_if{|k| k.to_sym == :price && persisted?}
+    super(args)
+  end
 
 
 
