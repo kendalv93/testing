c24a5400a16bf873ef828d769a802f6fccc46c7d
Kendal <vuek@augsburg.edu>
2015-12-21 16:29:34 -0600

    added error reader, prints out error messages

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     collection { post :import }
   end
 
-  root to: "forms#index"
+  root to: "forms_import#new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
