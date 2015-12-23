32b00d708cce2d2ccd30ed2870ed08cd14806142
Kendal <vuek@augsburg.edu>
2015-12-23 15:46:47 -0600

    Merge branch 'master' of https://github.com/kendalv93/testing
    Conflicts:
    	Gemfile
    	Gemfile.lock
    	app/controllers/forms_controller.rb
    	app/models/concerns/import.rb
    	app/models/cosmo_sigma_row.rb
    	app/models/cosmo_web_row.rb
    	app/views/forms/index.html.erb
    	config/routes.rb
    	db/schema.rb
    
    tried to add has_errors into cosmo_web

--- app/models/cosmo_web_row.rb
+++ app/models/cosmo_web_row.rb
@@ -1,6 +1,5 @@
 class CosmoWebRow < ActiveRecord::Base
   belongs_to :form
-
   include Import::InstanceMethods
   class << self #load as class methods
     include Import::ClassMethods
@@ -16,4 +15,20 @@ class CosmoWebRow < ActiveRecord::Base
 
     end
   end
+ 
+
+  def has_errors
+    headers = [prod_int_num, prod_num, web_desc, model_num, manufacturer, upc, part_num, prod_type, prod_terms,
+               factory_serviced, dropship, hype, upgrade_text, comp_line, comp_price,
+               was_price_line, was_price, adv_price_line, adv_price, price_add_on, sold_in_pairs, warr, mfr_reb, vc_reb, 
+               add_column2, add_column3, add_column4, add_column5, avail, alt_images, ent_dt, condition, var_opt, var_val, var_num, 
+               offer_num, cat_grp_num, acc_1, acc_2, acc_3, acc_4, up_1, up_2, up_3, cat_main_id_1, cat_main_name_1, cat_sub_id_1, 
+               cat_sub_name_1, cat_main_id_2, cat_main_name_2, cat_sub_id_2, cat_sub_name_2, cat_main_id_3, cat_main_name_3, 
+               cat_sub_id_3, cat_sub_name_3]
+    errors.add(:char_errors, "barf ") unless !illegal_character(headers)
+
+  end
+
+
+
 end
